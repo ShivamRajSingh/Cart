097c9ab59650ce386fb52c4421169ccaaf6ab2df
shivam3009 <shivamrajsingh3009@gmail.com>
2021-05-01 11:58:29 +0530

    props

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,43 @@ import React from 'react';
 import CartItem from './CartItem';
 
 class Cart extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            products : [
+                {
+                    price : 999,
+                    title : 'Mobile Phone',
+                    qty : 1,
+                    img : ''
+                },
+                {
+                    price : 99,
+                    title : 'Wrist Watch',
+                    qty : 1,
+                    img : ''
+                },
+                {
+                    price : 9999,
+                    title : 'Laptop',
+                    qty : 1,
+                    img : ''
+                }
+            ]
+        }
+    }
+
     render(){
+        // const arr = [1,2,3,4,5]
+        const {products} = this.state;
         return(
             <div className = "cart">
-                <CartItem />
-                <CartItem />
-                <CartItem />
+                {products.map((product) =>{
+                    return <CartItem product = {product} />
+                })}
+                {/* {arr.map((item) =>{
+                    return item + 5
+                })} */}
             </div>
         )
         
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,15 +1,6 @@
 import React from 'react';
 
 class CartItem extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            price : 999,
-            title : 'Mobile Phone',
-            qty : 1,
-            img : ''
-        }
-    }
     decreaseQuantity = () =>{
         // console.log('this.state', this.state)
         const {qty} = this.state
@@ -40,7 +31,7 @@ class CartItem extends React.Component{
         })
     }
     render(){
-        const {price , title , qty} = this.state;
+        const {price , title , qty} = this.props.product;
         return(
             <div className = "cart-item">
                  <div className = "left-block">    
